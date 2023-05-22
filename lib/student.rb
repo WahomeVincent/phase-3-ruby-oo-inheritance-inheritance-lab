bacd6e7e2389e178f09b5cff2f3d29c862ab09d8
Wahome <vinwahome@gmail.com>
2023-05-22 15:15:18 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
-
-end
+class Student < User
+    def initialize(first_name, last_name)
+      super(first_name, last_name)
+      @knowledge = []
+    end
+  
+    def learn(string)
+      @knowledge << string
+    end
+  
+    def knowledge
+      @knowledge
+    end
+  end
